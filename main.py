322845ba195d3b45cba5029279547354b22d82ce
extKikuchi <kikuchi@ext.cx>
2024-05-16 07:41:24 +0900

    update

--- main.py
+++ main.py
@@ -10,8 +10,14 @@ def load_rfid_numbers_from_file(file_path):
         rfid_numbers = [line.strip() for line in file.readlines() if line.strip()]
     return rfid_numbers
 
+def save_rfid_numbers_to_file(file_path, rfid_numbers):
+    with open(file_path, 'w') as file:
+        for rfid in rfid_numbers:
+            file.write(rfid + '\n')
+
 # 初期化時にテキストファイルからRFID番号を読み込む
-g_rfid_numbers_now = load_rfid_numbers_from_file('RFID.txt')
+file_path = 'RFID.txt'
+g_rfid_numbers_now = load_rfid_numbers_from_file(file_path)
 g_door_lock_status = True
 
 class GetRfidDatas:
@@ -28,6 +34,7 @@ class AddRfidData:
         global g_rfid_numbers_now
         new_rfid = generate_rfid_number()
         g_rfid_numbers_now.append(new_rfid)
+        save_rfid_numbers_to_file(file_path, g_rfid_numbers_now)
         msg = {
             "message": "RFID data added",
             "new_rfid": new_rfid
@@ -40,6 +47,7 @@ class RemoveRfidData:
         global g_rfid_numbers_now
         if g_rfid_numbers_now:
             removed_rfid = g_rfid_numbers_now.pop(0)  # FIFOで削除（最初の要素を削除）
+            save_rfid_numbers_to_file(file_path, g_rfid_numbers_now)
             msg = {
                 "message": "RFID data removed",
                 "removed_rfid": removed_rfid
@@ -55,7 +63,7 @@ class UnlockDoor:
     def on_post(self, req, resp):
         global g_door_lock_status
         g_door_lock_status = False
-        print("LockDoor called: g_door_lock_status set to", g_door_lock_status)
+        print("UnlockDoor called: g_door_lock_status set to", g_door_lock_status)
         msg = {
             "result": "unlocked"
         }
@@ -84,8 +92,8 @@ class LockStatus:
 
 api = falcon.App()
 api.add_route('/get-rfid-datas', GetRfidDatas())
-api.add_route('/unlock-door', UnlockDoor())
 api.add_route('/add-rfid-data', AddRfidData())
 api.add_route('/remove-rfid-data', RemoveRfidData())
+api.add_route('/unlock-door', UnlockDoor())
 api.add_route('/lock-door', LockDoor())
 api.add_route('/get-lock-status', LockStatus())
