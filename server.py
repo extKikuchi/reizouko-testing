31b2cedd9b5cf8b673ebcdc9d1dc344d146b6485
extKikuchi <96615918+extKikuchi@users.noreply.github.com>
2024-06-04 15:45:07 +0900

    add

---
+++ server.py
@@ -0,0 +1,44 @@
+import falcon
+import json
+import requests
+
+ANDROID_API_URL = 'http://<ANDROID_DEVICE_IP>:8080'  # ここにAndroidデバイスのIPアドレスを入力してください
+
+class GetRfidData:
+    def on_post(self, req, resp):
+        response = requests.post(f'{ANDROID_API_URL}/get-rfid-datas')
+        rfid_data = response.json()
+        resp.body = json.dumps(rfid_data)
+        resp.status = falcon.HTTP_200
+
+class UnlockDoor:
+    def on_post(self, req, resp):
+        response = requests.post(f'{ANDROID_API_URL}/unlock-door')
+        result = response.json()
+        resp.body = json.dumps(result)
+        resp.status = falcon.HTTP_200
+
+class LockDoor:
+    def on_post(self, req, resp):
+        response = requests.post(f'{ANDROID_API_URL}/lock-door')
+        result = response.json()
+        resp.body = json.dumps(result)
+        resp.status = falcon.HTTP_200
+
+class GetLockStatus:
+    def on_post(self, req, resp):
+        response = requests.post(f'{ANDROID_API_URL}/get-lock-status')
+        result = response.json()
+        resp.body = json.dumps(result)
+        resp.status = falcon.HTTP_200
+
+app = falcon.App()
+app.add_route('/get-rfid-datas', GetRfidData())
+app.add_route('/unlock-door', UnlockDoor())
+app.add_route('/lock-door', LockDoor())
+app.add_route('/get-lock-status', GetLockStatus())
+
+
+from wsgiref import simple_server
+server = simple_server.make_server('0.0.0.0', 8000, app)
+server.serve_forever()
